293a8f9a99d1f86db6de1da0b3300798755e8a2f
kisfelix1 <kisfelix1@gmail.com>
2022-09-22 19:10:48 +0200

    feature - create StatusStore svelte store

---
+++ stores/StatusStore.js
@@ -0,0 +1,19 @@
+import { writable } from "svelte/store";
+export const statuses = writable([]);
+export function makeStatusStore() {
+	const loading = writable(false);
+	const error = writable(false);
+	async function fetchStatuses(boardId) {
+		loading.set("Loading statuses");
+		error.set(false);
+		try {
+			const response = await fetch(`/api/${boardId}/statuses`);
+            const value = await response.json();
+			statuses.update(data => data.concat(value));
+		} catch(e) {
+			error.set(e);
+		}
+		loading.set(false);
+	}
+	return [statuses, loading, error, fetchStatuses];
+}
