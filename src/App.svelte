978c69e2087d7007055e69f7b227255d583462a4
kisfelix1 <kisfelix1@gmail.com>
2022-09-23 16:15:34 +0200

    feature - add create board functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,28 @@
 <script>
 	import {getBoards} from '../stores/BoardStore.js';
 	import Board from './Board.svelte';
+	import {postFetch, getFetch} from './FetchManager.js';
 	const [data, loading, error, get] = getBoards();
+	async function handleCreateBoard(){
+		let response = await postFetch("/api/create/board/0");
+		let newBoard = await getFetch(`/api/board/${response.id}`);
+		data.update(boards => boards.concat(newBoard));
+	}
 </script>
 <button on:click="{get}">
 	Show All Boards
 </button>
 
-<div class="board-container"><button id="create-board">Create new board</button>
-{#if $loading}
-	{$loading}
-{:else if $error}
-	{$error}
-{:else}
-	{#each $data as board}
-		 <Board boardId="{board.id}" title="{board.title}"></Board>
-	{/each}
-{/if}
+<div class="board-container">
+	<button id="create-board" on:click={handleCreateBoard}>Create new board</button>
+	{#if $loading}
+		{$loading}
+	{:else if $error}
+		{$error}
+	{:else}
+		{#each $data as board}
+			<Board boardId="{board.id}" title="{board.title}"></Board>
+		{/each}
+	{/if}
 </div>
   
