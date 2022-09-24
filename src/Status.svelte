1e659829d896891beab1b3e87a9c19d849e9daf2
kisfelix1 <kisfelix1@gmail.com>
2022-09-24 23:58:57 +0200

    feature - create status component

---
+++ src/Status.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Card from "./Card.svelte";
+    import {cards} from "../stores/CardStore.js";
+    export let boardId;
+    export let title;
+    export let position;
+</script>
+
+
+<div class="board-column">
+    <div class="board-column-title">{title}</div>    
+    <div class="board-column-content">
+        {#each $cards as card}
+            {#if card.board_id == boardId && card.position == position}
+                <Card title="{card.title}"></Card>
+            {/if}
+        {/each}
+    </div>
+</div>
