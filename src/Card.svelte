29e07efd922de66d040b4cab5849464967ca741d
kisfelix1 <kisfelix1@gmail.com>
2022-09-24 23:58:45 +0200

    feature - create card component

---
+++ src/Card.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let title;
+</script>
+
+<div class="card" draggable="true">
+    <div class="card-remove"><i class="fas fa-trash-alt" data-card-id="7" data-board-id="2"></i></div>
+    <div class="card-title">{title}</div>
+</div>
