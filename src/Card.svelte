7f9595087a8fbdf10cdd0b057a04a324e2f9192e
kisfelix1 <kisfelix1@gmail.com>
2022-09-25 10:21:24 +0200

    feature - implement card renaming

--- src/Card.svelte
+++ src/Card.svelte
@@ -1,10 +1,21 @@
 <script>
-    import {deleteCard} from '../stores/CardStore.js';
+    import {deleteCard, renameCard} from '../stores/CardStore.js';
     export let title;
     export let cardId;
+    let editingTitle = false;
+    function handleEditTitle(boardId){
+        editingTitle=false;
+        renameCard(cardId, title);
+    }
 </script>
 
 <div class="card" draggable="true">
     <div class="card-remove" on:click={()=> deleteCard(cardId)}><i class="fas fa-trash-alt" data-card-id="7" data-board-id="2"></i></div>
-    <div class="card-title">{title}</div>
+    <div class="card-title"  on:click={() => editingTitle = true}>
+        {#if editingTitle}
+            <input type="text" bind:value={title} on:blur={() => {handleEditTitle(cardId)}}>
+        {:else}
+            {title}
+        {/if}
+    </div>
 </div>
